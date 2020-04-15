c1c9567b7b785d7b652fdbe67973c67aec893bb5
saroj3k <11826423+saroj3k@users.noreply.github.com>
2020-04-15 16:04:22 -0400

    Building a modified version for Angular

--- src/org/clg/pipeline/build-ng.groovy
+++ src/org/clg/pipeline/build-ng.groovy
@@ -2,8 +2,8 @@ package org.clg.pipeline
 
 def start(def params) {
 
-  stage("Launch Maven Agent") {
-    node("maven") {
+  stage("Launch Node Agent") {
+    node("nodejs") {
       git url: "${params.pipelineCodeGitUrl}", branch: "${params.pipelineCodeGitBranch}"
       build(params)
     }
@@ -22,65 +22,31 @@ def build(def params) {
           sh "git config http.sslVerify false"
           git url: "${params.gitUrl}", branch: "${params.gitBranch}", credentialsId: "${namespace}-${params.gitSecret}"
         }
-      }
+      } //stage checkout
 
-      stage('Build From Source') {
-        sh "mvn -B -Dmaven.wagon.http.ssl.insecure=true -s /maven-conf/settings.xml -P ocp -DappName=app package"
-      }
+      stage ('install modules'){
+          sh '''
+            npm install --verbose -d
+            npm install --save classlist.js
+          '''
+        } //stage install
 
-      stage("Process OpenShift Config") {
-        String[] requiredFields = ["readinessProbe", "livelinessProbe", "secretKeyRef", "configMapRef"]
-	      String[] positiveIntFields = ["replicas"]
-        def config = readYamlFile("./ocp/config.yml", "A config.yml file is required in the path ./ocp/")
+       //defaults to prod build
+       stage ('build') {
+            sh '$(npm bin)/ng build --prod --build-optimizer'
+          } //stage build
 
-	      def err = ""
-	      requiredFields.each {
-		      if(config["${it}"] == null || config["${it}"] == "") {
-			      err << "${it} is a required field\n"
-		      }
-	      }
-
-	      positiveIntFields.each {
-		      if(config["${it}"] != null && config["${it}"] < 0) {
-			      err.append("${it} must be a positive integer\n")
-		      }
-	      }
-
-	      if(err.toString() != "") {
-		      throw new RuntimeException(err.toString())
-	      }
-        def ocPatch = """ patch dc/${params.appName}  -p  '{"spec": {"template": {"spec": {"containers": [{"name": "${params.appName}", "envFrom": null}] }}}}'  """
-        openshift.raw(ocPatch)
-        ocPatch = """ patch dc/${params.appName}  -p  '{"spec": {"template": {"spec": {"containers": [{"name": "${params.appName}", "envFrom": [{"configMapRef": {"name": "${config.configMapRef}"}}, {"secretRef": {"name": "${config.secretKeyRef}"}}]}]}}}}'  """
-        openshift.raw(ocPatch)
-        openshift.raw("apply -f ./ocp/dev/${config.configMapRef}.yml")
-        openshift.raw("apply -f ./ocp/dev/${config.secretKeyRef}.yml")
-      }
-
-			stage('Build & Deploy Image') {
-        timeout(time:5, unit:'MINUTES'){
-          def pom = readMavenPom file: "pom.xml"
-          def appVersion = "${pom.version}"
-          def commitVersion = sh(returnStdout: true, script: 'git rev-parse --short HEAD').trim()
-          def commitHash = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
-          openshift.raw("set env bc/${params.appName}-jvm-docker GIT_REF=${commitHash} GIT_URL=${params.gitUrl} GIT_BRANCH=${params.gitBranch} GIT_COMMIT=${commitVersion}")
-          def bc = openshift.selector("bc/${params.appName}-jvm-docker")
-          bc.startBuild("--from-dir='target'")
-          bc.logs("-f")
-          def newIsLabels = openshift.selector("is", "${params.appName}").object()
-          newIsLabels.metadata.labels["lastest_commit"] = commitVersion
-          newIsLabels.metadata.labels["committer_name"] = env.GIT_COMMITTER_NAME
-          newIsLabels.metadata.labels["author"] = 'Jenkins'
-          newIsLabels.metadata.labels["latest_version"] = appVersion
-          openshift.apply(newIsLabels)
-          def dc  = openshift.selector("dc", "${params.appName}")
-          dc.rollout().latest()
-          dc.rollout().status()
-        }
-      }
-    }
-  }
-}
+          //build image
+       stage ('build image') {
+            sh '''
+              mkdir dist/nginx-cfg
+              cp nginx/status.conf dist/nginx-cfg
+              oc start-build angular-5-example-rhel --from-dir=dist --follow
+            '''
+          } //stage build image	    
+      } //openshift withProject
+    } //openshift withCluster
+  } //build
 
 def readYamlFile(def filePath, String errMessage) {
 	try {
